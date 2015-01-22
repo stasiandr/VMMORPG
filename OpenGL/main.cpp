9e516d6b5264c60be63603df506cc7adf76688bc
stndart <sstndart@gmail.com>
2015-01-22 17:09:31 +0300

    скорее всего баги в камере

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -19,16 +19,16 @@ using namespace std;
 void display(void);
 void reshape(int, int);
 
-int wid = 16;
-int hei = 256;
-int dee = 16;
+int wid = 25;
+int hei = 25;
+int dee = 25;
 
-bool good(int i, int j, int k, int siz)
+bool bad(int i, int j, int k, int siz)
 {
     if (i >= 0 && j >= 0 && k >= 0)
         if (i*hei*dee + j*dee + z < siz)
-            return true;
-    return false;
+            return false;
+    return true;
 }
 
 GLfloat rtri;
@@ -42,52 +42,52 @@ void reshape(int, int);
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
-    
+
     /* set the window size to 512 x 512 */
     glutInitWindowSize(1024, 630);
-    
+
     /* set the display mode to Red, Green, Blue and Alpha
      allocate a depth buffer
      enable double buffering
      */
     glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
-    
+
     /* create the window (and call it Lab 1) */
     glutCreateWindow("Lab 1");
-    
+
     /* set the glut display callback function
      this is the function GLUT will call every time
      the window needs to be drawn
      */
     glutDisplayFunc(display);
-    
+
     /* set the glut reshape callback function
      this is the function GLUT will call whenever
      the window is resized, including when it is
      first created
      */
     glutReshapeFunc(reshape);
-    
+
     glutIgnoreKeyRepeat(1);
     glutKeyboardFunc(processNormalKeys);
     glutKeyboardUpFunc(processNormalKeysRelease);
     glutSpecialFunc(pressKey);
     glutSpecialUpFunc(releaseKey);
-    
+
     glEnable(GL_CULL_FACE);
-    
+
     /* set the default background color to black */
     glClearColor(0,0,0,1);
     glutMouseFunc(mouseButton);
     glutMotionFunc(mouseMove);
-    
-    
-    
+
+
+
     /* enter the main event loop so that GLUT can process
      all of the window event messages
      */
     glutMainLoop();
-    
+
     return 0;
 }
 
@@ -105,34 +105,35 @@ void display(void)
 {
     if (deltaMove || deltaMove_Sides || deltaMove_Y)
         computePos(deltaMove, deltaMove_Sides, deltaMove_Y);
-    
+
     /* clear the color buffer (resets everything to black) */
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    
+
     /* set the current drawing color to red */
-    
-    
+
+
     glLoadIdentity();
-    
+
     glEnable(GL_DEPTH_TEST);
     glDisable(GL_BLEND);
-    
+
     glTranslatef(0.0f, 0.0f,0.0f);
-    
+
     gluLookAt(	x, y, z,
               x+lx, y+ly,  z+lz,
               0.0f, 1.0f,  0.0f);
-    
+
     //---
-    
+
     ifstream in;
-    in.open("/Users/irina/VMMORPG/chunck.txt");
+    in.open("chunck.txt");
 
     string is;
     in >> is;
-  //  std::cout << is;
     in.close();
-    
+
+    cout << "AAA ! " << bad(24, 23, 17, is.size()) << endl;
+
     glPushMatrix();
     glRotatef(rtri, 1.0f, 1.0f, 1.0f);
     glBegin(GL_QUADS);
@@ -140,48 +141,47 @@ void display(void)
         for (int j = 0; j < hei; j++)
             for (int k = 0; k < dee; k++)
                 if (is[i*hei*dee + j*dee + k] == '1')
-                {
-                    /*
-                    if (!(good((i-1)*hei*dee, j*dee, z, is.size())) || is[(i-1)*hei*dee + j*dee + z] == '0')
+                {/*
+                    if (bad(i-1, j, k, is.size()))
                         drawModel::plain_side(j, k, j+1, k+1, i);
-                    if (!(good((i+1)*hei*dee, j*dee, z, is.size())) || is[(i+1)*hei*dee + j*dee + z] == '0')
-                        drawModel::plain_side_reversed(j, k, j+1, k+1, i+1);*/
-                    if (!(good(i*hei*dee, (j-1)*dee, z, is.size())))
+                    if (bad(i+1, j, k, is.size()))
+                        drawModel::plain_side_reversed(j, k, j+1, k+1, i); // Тут маленький баг!!!*/
+                    if (bad(i, j-1, k, is.size()))
+                    {
                         drawModel::plain_top_reversed(i, k, i+1, k+1, j);
-                    if (!(good(i*hei*dee, (j+1)*dee, z, is.size())))
-                        drawModel::plain_top(i, k, i+1, k+1, j);
+                        if (j != 0)
+                            cout << i << ' ' << j << ' ' << k << endl;
+                    }
                 }
 
-    drawModel::plain_side(0, 0, 1, 1, 0);
-    
     glEnd();
-    
+
     glPopMatrix();
-    
+
     glPushMatrix();
     glBegin(GL_LINES);
     glColor3f(1, 1, 1);
-    
+
     glVertex3f(-1000, 0, 0);
     glVertex3f(1000, 0, 0);
-    
+
     glVertex3f(0, -1000, 0);
     glVertex3f(0, 1000, 0);
-    
+
     glVertex3f(0, 0, -1000);
     glVertex3f(0, 0, 1000);
-    
+
     glEnd();
-    
+
     glPopMatrix();
 
-    
+
         /* swap the back and front buffers so we can see what we just drew */
     glutSwapBuffers();
-    
+
 //    float acc = 0.01f;
 //    vel += acc;
-    
+
     glutPostRedisplay();
 }
 
@@ -198,16 +198,16 @@ void reshape(int width, int height)
      same size as the window
      */
     glViewport(0,0,width,height);
-    
+
     /* switch to the projection matrix */
     glMatrixMode(GL_PROJECTION);
-    
+
     /* clear the projection matrix */
     glLoadIdentity();
-    
+
     /* set the camera view, orthographic projection in 2D */
     gluPerspective( 45.0f, (GLfloat)width/(GLfloat)height, 0.1f, 400.0f );
-    
+
     /* switch back to the model view matrix */
     glMatrixMode(GL_MODELVIEW);
 }
