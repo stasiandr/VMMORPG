65e0fe60071ed0858938fb5c47ba18a5967cda83
stndart <sstndart@gmail.com>
2015-02-12 18:10:03 +0300

    Normal chunks creator

--- OpenGL/1.py
+++ OpenGL/1.py
@@ -1,4 +1,16 @@
-f = open("chunck.txt", "w+")
-for i in range(256*16*16):
-	f.write("1")
-f.close()
+def ten(n):
+    return '0'*(4-len(str(n)))+str(n)
+h, d = map(int, input('Number of chunks: ').split())
+a, b, c = map(int, input("Sizes of chunks: ").split())
+for i in range(h):
+    for j in range(d):
+        o = open(str(i)+str(j)+'.VMC', 'w')
+        o.write(ten(i)+ten(0)+ten(j))
+        for x in range(a):
+            for y in range(b):
+                for z in range(c):
+                    if y < 5 or (y == 6 and x == 2 and z == 3):
+                        o.write('1')
+                    else:
+                        o.write('0')
+        o.close()
