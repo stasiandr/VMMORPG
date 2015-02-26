e3587869c6d6afa1b8f4d52e29dc30be20e72679
stndart <sstndart@gmail.com>
2015-02-26 19:06:09 +0300

    Что-то вроде инициализации текстуры

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -53,6 +53,8 @@ int okr(float n)
         return s;
 }
 
+GLuint Texture;
+
 float * posses;
 float st[3];
 bool blocker [6];
@@ -67,6 +69,61 @@ void reshape(int, int);
 
 const char * fns[16];
 string iss[16];
+
+GLuint loadBMP_custom(const char * imagepath)
+{
+    // Данные, прочитанные из заголовка BMP-файла
+    unsigned char header[54]; // Каждый BMP-файл начинается с заголовка, длиной в 54 байта
+    unsigned int dataPos;     // Смещение данных в файле (позиция данных)
+    unsigned int width, height;
+    unsigned int imageSize;   // Размер изображения = Ширина * Высота * 3
+    // RGB-данные, полученные из файла
+    unsigned char * data;
+    FILE * file = fopen(imagepath,"rb");
+    if (!file) {
+      printf("Изображение не может быть открыто\n");
+      return 0;
+    }
+    if ( fread(header, 1, 54, file) != 54 ) { // Если мы прочитали меньше 54 байт, значит возникла проблема
+        printf("Некорректный BMP-файл\n");
+        return false;
+    }
+    if ( header[0]!='B' || header[1]!='M' ){
+        printf("Некорректный BMP-файл\n");
+        return 0;
+    }
+    // Читаем необходимые данные
+    dataPos    = *(int*)&(header[0x0A]); // Смещение данных изображения в файле
+    imageSize  = *(int*)&(header[0x22]); // Размер изображения в байтах
+    width      = *(int*)&(header[0x12]); // Ширина
+    height     = *(int*)&(header[0x16]); // Высота
+    // Некоторые BMP-файлы имеют нулевые поля imageSize и dataPos, поэтому исправим их
+    if (imageSize==0)    imageSize=width*height*3; // Ширину * Высоту * 3, где 3 - 3 компоненты цвета (RGB)
+    if (dataPos==0)      dataPos=54; // В таком случае, данные будут следовать сразу за заголовком
+    // Создаем буфер
+    data = new unsigned char [imageSize];
+
+    // Считываем данные из файла в буфер
+    fread(data,1,imageSize,file);
+
+    // Закрываем файл, так как больше он нам не нужен
+    fclose(file);
+
+    // Создадим одну текстуру OpenGL
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+
+    // Сделаем созданную текстуру текущий, таким образом все следующие функции будут работать именно с этой текстурой
+    glBindTexture(GL_TEXTURE_2D, textureID);
+
+    // Передадим изображение OpenGL
+    glTexImage2D(GL_TEXTURE_2D, 0,GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+    return textureID;
+}
 
 void Update()
 {
@@ -142,6 +199,7 @@ int main(int argc, char** argv)
     glutMouseFunc(mouseButton);
     glutMotionFunc(mouseMove);
 
+    GLuint Texture = loadBMP_custom("uvtemplate.bmp");
 
     /* enter the main event loop so that GLUT can process
      all of the window event messages
@@ -150,7 +208,6 @@ int main(int argc, char** argv)
 
     return 0;
 }
-
 
 
 
@@ -278,6 +335,7 @@ void chunks(string is)
     float tx = posses[0] - 0.5f;
     float ty = posses[1] - 1.5f;
     float tz = posses[2] - 0.5f;
+
     for (int i = 0; i < wid; i++)
         for (int j = 0; j < hei; j++)
             for (int k = 0; k < dee; k++)
