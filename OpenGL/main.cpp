afe928794611ef344e35da12096a637505b8be2f
stasiandr <stasiandr@gmail.com>
2015-03-17 12:38:49 +0300

    Working textures

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -43,6 +43,62 @@ GLuint texture[1];
 	glTexImage2D(GL_TEXTURE_2D, 0, 3, texture1->sizeX, texture1->sizeY, 0,
     GL_RGB, GL_UNSIGNED_BYTE, texture1->data);
 }*/
+
+GLuint loadBMP_custom(const char * imagepath)
+{
+    // Данные, прочитанные из заголовка BMP-файла
+    unsigned char header[54]; // Каждый BMP-файл начинается с заголовка, длиной в 54 байта
+    unsigned int dataPos;     // Смещение данных в файле (позиция данных)
+    unsigned int width, height;
+    unsigned int imageSize;   // Размер изображения = Ширина * Высота * 3
+    // RGB-данные, полученные из файла
+    unsigned char * data;
+    FILE * file = fopen(imagepath,"rb");
+    if (!file) {
+      printf("Изображение не может быть открыто\n");
+      return 0;
+    }
+    if ( fread(header, 1, 54, file) != 54 ) { // Если мы прочитали меньше 54 байт, значит возникла проблема
+        printf("Некорректный BMP-файл\n");
+        return false;
+    }
+    if ( header[0]!='B' || header[1]!='M' ){
+        printf("Некорректный BMP-файл\n");
+        return 0;
+    }
+    // Читаем необходимые данные
+    dataPos    = *(int*)&(header[0x0A]); // Смещение данных изображения в файле
+    imageSize  = *(int*)&(header[0x22]); // Размер изображения в байтах
+    width      = *(int*)&(header[0x12]); // Ширина
+    height     = *(int*)&(header[0x16]); // Высота
+    // Некоторые BMP-файлы имеют нулевые поля imageSize и dataPos, поэтому исправим их
+    if (imageSize==0)    imageSize=width*height*3; // Ширину * Высоту * 3, где 3 - 3 компоненты цвета (RGB)
+    if (dataPos==0)      dataPos=54; // В таком случае, данные будут следовать сразу за заголовком
+    // Создаем буфер
+    data = new unsigned char [imageSize];
+
+    // Считываем данные из файла в буфер
+    fread(data,1,imageSize,file);
+
+    // Закрываем файл, так как больше он нам не нужен
+    fclose(file);
+
+    // Создадим одну текстуру OpenGL
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+
+    // Сделаем созданную текстуру текущий, таким образом все следующие функции будут работать именно с этой текстурой
+    glBindTexture(GL_TEXTURE_2D, textureID);
+
+    // Передадим изображение OpenGL
+    glTexImage2D(GL_TEXTURE_2D, 0,GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+    return textureID;
+}
+
 void Update();
 
 void display(void);
@@ -158,7 +214,7 @@ int main(int argc, char** argv)
     /* create the window (and call it Lab 1) */
     glutCreateWindow("Lab 1");
 
-    //LoadGLTextures("uvtemplate.bmp");			// Загрузка текстур
+    LoadBMP_custom("uvtemplate.bmp");			// Загрузка текстур
     glEnable(GL_TEXTURE_2D);
 
     /* set the glut display callback function
