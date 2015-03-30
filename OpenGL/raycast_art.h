30745b8951c22363ddbc7d6e7f90b78762f6bd16
stndart <sstndart@gmail.com>
2015-03-30 17:48:58 +0300

    Last working landscapes etc.

---
+++ OpenGL/raycast_art.h
@@ -0,0 +1,28 @@
+// raycast_art.h
+
+using namespace std;
+
+for (i = 0 ; i < 8 ; i++)
+{
+ x0 = int(x) + 1;
+ y0 = int(y) + 1;
+ z0 = int(z) + 1;
+ r1 = x0 / cos(an) / cos(yan);
+ r2 = y0 / sin(yan);
+ r3 = z0 / sin(an) / cos(yan);
+ if (r1 < r2 && r1 < r3)
+ {
+    y0 = r1 * sin(yan)
+    z0 = r1 * cos(yan) * cos(an)
+    xb = int(x0)
+    yb = int(y0)
+    zb = int(z0)
+    if (iss[ch][tex*hei*dee + tey*dee + tez + 12] == '1')
+    {
+        rx = xb + int(x)
+        ry = yb + int(y)
+        rz = zb + int(z)
+
+    }
+ }
+}
